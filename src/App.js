7d2fce64d78d017ee69409eb08ea8641cf53c1ac
codewithgi <118617546+codewgi@users.noreply.github.com>
2024-01-07 14:13:45 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './css/style.css';
-import { useState } from 'react';
 import QRCode from 'react-qr-code';
+import { useState } from 'react';
 
 function App() {
   const [value, setValue] = useState(null);
@@ -13,21 +13,23 @@ function App() {
 
   return (
     <div className="w-full min-h-screen grid place-content-center bg-[#181818]">
-        <div className='w-[23rem] p-7 space-y-8 text-slate-300'>
-            {value?
-              <QRCode value={value} style={{ height: "auto", maxWidth: "100%", width: "100%" }}/>
-              :
-              <div className='w-full h-[18rem] border border-neutral-200 rounded-3xl grid  place-content-center bg-[#202020]'>
-                <span>None</span>
-              </div>
-            }
-            <form className='space-y-5' onSubmit={handler}>
-              <div>
-                <input className='bg-[#252525] rounded w-full p-2 focus:outline outline-neutral-300' placeholder='text or yourlink.com' id='input-text'></input>
-              </div>
-              <button className='w-full py-3 flex justify-center bg-[#252525] hover:bg-neutral-600 duration-200 rounded'>Create</button>
-            </form>
-        </div>
+      <div className='w-[23rem] p-7 space-y-8 text-slate-300'>
+          {value?
+            <QRCode value={value} style={{ height : "auto", maxWidth: "100%", width:"100%" }}/>
+            :
+            <div className='w-full h-[18rem] border border-neutral-200 rounded-3xl grid place-content-center bg-[#202020]'>
+              <span>None</span>
+            </div>
+          }
+          <form className='space-y-5' onSubmit={handler}>
+            <div>
+              <input className='bg-[#252525] rounded w-full p-2' placeholder='text or yourlink.com'></input>
+            </div>
+            <button className='w-full py-3 flex justify-center bg-[#252525] hover:bg-neutral-600 duration-200 rounded'>
+              Create
+            </button>
+          </form>
+      </div>
     </div>
   );
 }
